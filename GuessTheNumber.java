06292a44cc64f701c83252112fad433622b96572
shalomshaliii <shalomsolomon977@gmail.com>
2024-09-09 16:14:45 -0700

    this a betting game

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,1 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
 
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        Random random = new Random();
+        int secretnumber = random.nextInt(100) + 1;
+        int guess;
+        int playerbet;
+        int balance = 100;
+        int attempts = 3;
+        System.out.println("welcome to the number guessing game");
+        System.out.println("you will start with a balance of $100");
+        System.out.println("you have 3 attempts to guess the number");
+        while (attempts > 0) {
+            System.out.println("your current balance is:" + balance);
+            System.out.println("how much will you bet? or if you are afraid of losing you can enter 0");
+            playerbet = obj.nextInt();
+            if (playerbet == 0) {
+                System.out.println("well I am really sorry that you are a quiter, and quiter balance is:" + balance);
+                break;
+            }
+            if (playerbet > balance) {
+                System.out
+                        .println("I am sorry,you are broke to bet that much money my friend try to bet on your level");
+                continue;
+            }
+            System.out.println("Guess a number from 1 upto 100");
+            guess = obj.nextInt();
+            attempts--;
+            if (guess == secretnumber) {
+                System.out.println("you are lucky my friend you have guessed the correct number,and you have won $:"
+                        + playerbet * 2);
+                balance += playerbet * 2;
+                System.out.println("you have" + attempts + "attempts remaining");
+                break;
+
+            } else {
+                if (guess < secretnumber) {
+                    System.out.println("too low!,try again");
+                } else {
+                    System.out.println("too high!,try again");
+                }
+                System.out.println("you have" + attempts + "attempts remaining");
+            }
+
+            if (attempts == 0) {
+                System.out.println("you have run out of attempts! The number was" + secretnumber);
+                balance -= playerbet;
+                System.out.println("your final balance is:" + balance);
+
+            }
+
+        }
+    }
+}
--- ToDOList.java
+++ ToDOList.java
@@ -1,1 +1,58 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class ToDOList {
+    public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Boolean> taskstatus = new ArrayList<>();
+        Scanner obj = new Scanner(System.in);
+        while (true) {
+            System.out.println("1.Add Tasks");
+            System.out.println("2. view Tasks");
+            System.out.println("3. Mark as Done");
+            System.out.println("4. Quit");
+            System.out.println("Enter your choice");
+            int choice = obj.nextInt();
+            if (choice == 1) {
+                System.out.println("Add your tasks");
+                String task = obj.next();
+                tasks.add(task);
+            } else if (choice == 2) {
+                if (tasks.isEmpty()) {
+                    System.out.println("No tasks available");
+
+                } else {
+                    for (String task : tasks) {
+                        System.out.println(task);
+                    }
+                }
+
+            } else if (choice == 3) {
+                if (tasks.isEmpty()) {
+                    System.out.println("No tasks available.");
+                    break;
+                }
+                System.out.println("Tasks:");
+                ArrayList<String> taskStatus;
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(
+                            (i + 1) + ". " + tasks.get(i) + " - " + (taskStatus.get(i) != null ? "Done" : "Not Done"));
+                }
+                System.out.print("Enter the task number to mark as done: ");
+                int doneTaskNumber = obj.nextInt();
+                if (doneTaskNumber > 0 && doneTaskNumber <= tasks.size()) {
+                    // taskStatus.set(doneTaskNumber - 1, true);
+                    System.out.println("Task marked as done.");
+                } else {
+                    System.out.println("Invalid task number.");
+                }
+                break;
+            } else if (choice == 4) {
+                break;
+            } else {
+                System.out.println("invalid choice");
+            }
+        }
+
+    }
+}
